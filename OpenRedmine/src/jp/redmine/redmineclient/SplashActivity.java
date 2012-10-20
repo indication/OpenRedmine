f38c461422e9223bf2a3dc2ac56d32a17895c97f
in_dow <in_dow@hotmail.com>
2012-10-20 20:12:45 +0900

    add splash window

---
+++ OpenRedmine/src/jp/redmine/redmineclient/SplashActivity.java
@@ -0,0 +1,38 @@
+package jp.redmine.redmineclient;
+
+import jp.redmine.redmineclient.entity.RedmineConnection;
+import jp.redmine.redmineclient.model.ConnectionModel;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splash);
+		Handler hdl = new Handler();
+		hdl.postDelayed(new Runnable() {
+
+			private boolean hasList(){
+				ConnectionModel modelConnection = new ConnectionModel(getApplicationContext());
+				int count = 0;
+				for(@SuppressWarnings("unused") RedmineConnection con : modelConnection.fetchAllData()){
+					count++;
+				}
+				return count>0;
+			}
+
+			public void run() {
+				//@todo: first time.
+				Intent i = new Intent(getApplication(), hasList() ? ConnectionListActivity.class : ConnectionActivity.class);
+				startActivity(i);
+				SplashActivity.this.finish();
+			}
+		}, 500);
+
+	}
+}

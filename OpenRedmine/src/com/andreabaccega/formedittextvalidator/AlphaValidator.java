ad0442e5f996dae775c1dbbaa93586d71491d4dc
in_dow <in_dow@hotmail.com>
2012-10-21 00:03:40 +0900

    relocate external libraries

--- OpenRedmine/src/com/andreabaccega/formedittextvalidator/AlphaValidator.java
+++
@@ -1,7 +0,0 @@
-package com.andreabaccega.formedittextvalidator;
-
-public class AlphaValidator extends RegexpValidator {
-	public AlphaValidator(String message) {
-		super(message, "[a-zA-Z \\./-]*");
-	}
-}

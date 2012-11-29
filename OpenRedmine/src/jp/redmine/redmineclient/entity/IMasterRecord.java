1dbbbe98e7990081d520a107a90c2782bdb84c6a
in_dow <in_dow@hotmail.com>
2012-11-30 01:12:47 +0900

    add common interface about models

---
+++ OpenRedmine/src/jp/redmine/redmineclient/entity/IMasterRecord.java
@@ -0,0 +1,11 @@
+package jp.redmine.redmineclient.entity;
+
+public interface IMasterRecord {
+
+	public void setId(Long id);
+	public Long getId();
+	public void setRemoteId(Long id);
+	public Long getRemoteId();
+	public void setName(String name);;
+	public String getName();
+}

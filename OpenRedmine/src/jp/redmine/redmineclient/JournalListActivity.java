95804e2d6e2642e7c0396dfeb88b845fc30ca5ed
in dow <in_dow@hotmail.com>
2012-12-31 17:43:49 +0900

    first add the activity(imcomplete)

---
+++ OpenRedmine/src/jp/redmine/redmineclient/JournalListActivity.java
@@ -0,0 +1,45 @@
+package jp.redmine.redmineclient;
+
+import jp.redmine.redmineclient.form.RedmineIssueViewForm;
+import jp.redmine.redmineclient.intent.IssueIntent;
+import jp.redmine.redmineclient.model.IssueModel;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JournalListActivity extends Activity  {
+	public JournalListActivity(){
+		super();
+	}
+
+	private IssueModel modelIssue;
+	private RedmineIssueViewForm form;
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if(modelIssue != null){
+			modelIssue.finalize();
+			modelIssue = null;
+		}
+	}
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.journallist);
+
+		form = new RedmineIssueViewForm(this);
+
+
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		IssueIntent intent = new IssueIntent(getIntent());
+		int connectionid = intent.getConnectionId();
+		int issueid = intent.getIssueId();
+		modelIssue = new IssueModel(getApplicationContext(), connectionid,null);
+		form.setValue(modelIssue.fetchItem(issueid));
+	}
+}

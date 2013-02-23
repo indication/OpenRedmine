068bcb01a921cbd21dee8d84a36f8b07f9a3b515
in_dow <in_dow@hotmail.com>
2013-02-23 14:42:49 +0900

    fixes restored changes

--- OpenRedmine/src/jp/redmine/redmineclient/form/RedmineIssueViewForm.java
+++ OpenRedmine/src/jp/redmine/redmineclient/form/RedmineIssueViewForm.java
@@ -4,6 +4,9 @@ import jp.redmine.redmineclient.R;
 import jp.redmine.redmineclient.entity.RedmineIssue;
 import android.app.Activity;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -27,6 +30,9 @@ public class RedmineIssueViewForm extends FormHelper {
 		textIssueId = (TextView)view.findViewById(R.id.textIssueId);
 		textSubject = (TextView)view.findViewById(R.id.textSubject);
 		list = (ListView)view.findViewById(R.id.list);
+		layoutTitleContent = (LinearLayout)view.findViewById(R.id.layoutTitleContent);
+		layoutTitle1 = (ViewGroup)view.findViewById(R.id.layoutTitle1);
+		layoutTitle2 = (ViewGroup)view.findViewById(R.id.layoutTitle2);
 
 		viewHeader = view.getLayoutInflater().inflate(R.layout.issueviewdetail,null);
 		viewFooter = view.getLayoutInflater().inflate(R.layout.listview_footer,null);
@@ -38,6 +44,26 @@ public class RedmineIssueViewForm extends FormHelper {
 
 	public void setListHeaderViewVisible(boolean isVisible){
 		viewHeader.setVisibility(isVisible ? View.VISIBLE : View.GONE);
+	}
+	public void setupEvents(){
+		textSubject.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(layoutTitle1.getVisibility() == View.VISIBLE){
+					layoutTitle1.setVisibility(View.GONE);
+					layoutTitle1.removeAllViews();
+					layoutTitle2.addView(layoutTitleContent);
+					layoutTitle2.setVisibility(View.VISIBLE);
+				} else {
+					layoutTitle2.setVisibility(View.GONE);
+					layoutTitle2.removeAllViews();
+					layoutTitle1.addView(layoutTitleContent);
+					layoutTitle1.setVisibility(View.VISIBLE);
+				}
+
+			}
+		});
 	}
 
 	public void setListFooterViewVisible(boolean isVisible){

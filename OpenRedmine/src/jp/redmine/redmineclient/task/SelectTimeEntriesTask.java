5cbe29c7ca896eb2d2559a7b88472f97e64afe67
in_dow <in_dow@hotmail.com>
2013-04-04 01:59:35 +0900

    Add task for fetch timeentry

---
+++ OpenRedmine/src/jp/redmine/redmineclient/task/SelectTimeEntriesTask.java
@@ -0,0 +1,77 @@
+package jp.redmine.redmineclient.task;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
+import jp.redmine.redmineclient.db.cache.RedmineTimeEntryModel;
+import jp.redmine.redmineclient.entity.RedmineConnection;
+import jp.redmine.redmineclient.entity.RedmineTimeEntry;
+import jp.redmine.redmineclient.parser.DataCreationHandler;
+import jp.redmine.redmineclient.parser.ParserTimeEntry;
+import jp.redmine.redmineclient.url.RemoteUrlTimeEntries;
+
+public class SelectTimeEntriesTask extends SelectDataTask<Void> {
+	private final static int LIMIT = 50;
+
+	protected DatabaseCacheHelper helper;
+	protected RedmineConnection connection;
+	public SelectTimeEntriesTask(DatabaseCacheHelper helper,RedmineConnection con){
+		this.helper = helper;
+		this.connection = con;
+	}
+
+
+	public SelectTimeEntriesTask() {
+	}
+
+	@Override
+	protected Void doInBackground(Integer... params) {
+		final RedmineTimeEntryModel model = new RedmineTimeEntryModel(helper);
+		final ParserTimeEntry parser = new ParserTimeEntry();
+		parser.registerDataCreation(new DataCreationHandler<RedmineConnection,RedmineTimeEntry>() {
+			public void onData(RedmineConnection con,RedmineTimeEntry data) throws SQLException {
+				model.refreshItem(con,data);
+			}
+		});
+		SelectDataTaskDataHandler handler = new SelectDataTaskDataHandler() {
+			@Override
+			public void onContent(InputStream stream)
+					throws XmlPullParserException, IOException, SQLException {
+				helperSetupParserStream(stream,parser);
+				parser.parse(connection);
+			}
+		};
+
+		SelectDataTaskConnectionHandler client = new SelectDataTaskRedmineConnectionHandler(connection);
+		RemoteUrlTimeEntries url = new RemoteUrlTimeEntries();
+		for(int item : params){
+			int offset = 0;
+			url.filterLimit(LIMIT);
+			url.filterIssue(String.valueOf(item));
+			do {
+				url.filterOffset(offset);
+				fetchData(client,connection, url, handler);
+				offset += parser.getCount() + 1;
+			} while(parser.getCount() == LIMIT);
+		}
+		client.close();
+		return null;
+	}
+
+	@Override
+	protected void onErrorRequest(int statuscode) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	protected void onProgress(int max, int proc) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

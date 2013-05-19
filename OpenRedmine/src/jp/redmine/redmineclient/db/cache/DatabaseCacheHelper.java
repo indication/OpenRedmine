d65d2950c28e244239dfb105b372d5a3e8f0157c
in_dow <in_dow@hotmail.com>
2013-05-20 07:31:59 +0900

    Add closed column

--- OpenRedmine/src/jp/redmine/redmineclient/db/cache/DatabaseCacheHelper.java
+++ OpenRedmine/src/jp/redmine/redmineclient/db/cache/DatabaseCacheHelper.java
@@ -27,7 +27,7 @@ import com.j256.ormlite.table.TableUtils;
 
 public class DatabaseCacheHelper extends OrmLiteSqliteOpenHelper {
 	private static String DB_NAME="OpenRedmineCache.db";
-	private static int DB_VERSION=4;
+	private static int DB_VERSION=5;
 
     public DatabaseCacheHelper(Context context) {
     	super(context, getDatabasePath(context), null, DB_VERSION);
@@ -77,6 +77,10 @@ public class DatabaseCacheHelper extends OrmLiteSqliteOpenHelper {
 				addColumn(db,RedmineProject.class,"parent INTEGER");
 				TableUtils.createTable(source, RedmineTimeEntry.class);
 				TableUtils.createTable(source, RedmineTimeActivity.class);
+			case 4:
+				if(older > 2){
+					addColumn(db,RedmineIssue.class,"closed TEXT");
+				}
 				break;
 			}
 

ffc01a42a8c770a43c191f163e2c2c38ff2d93ee
in_dow <in_dow@hotmail.com>
2013-05-21 02:46:46 +0900

    Refactor post item

--- OpenRedmine/src/jp/redmine/redmineclient/task/SelectTimeEntriesPost.java
+++ OpenRedmine/src/jp/redmine/redmineclient/task/SelectTimeEntriesPost.java
@@ -2,33 +2,16 @@ package jp.redmine.redmineclient.task;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 import java.sql.SQLException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.entity.StringEntity;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xmlpull.v1.XmlPullParserException;
 
-import android.util.Log;
-
-import jp.redmine.redmineclient.BuildConfig;
 import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
 import jp.redmine.redmineclient.entity.RedmineConnection;
 import jp.redmine.redmineclient.entity.RedmineTimeEntry;
 import jp.redmine.redmineclient.url.RemoteUrlTimeEntries;
 
-public class SelectTimeEntriesPost extends SelectDataTask<Void,RedmineTimeEntry> {
+public class SelectTimeEntriesPost extends SelectDataPost<Void,RedmineTimeEntry> {
 
 	protected DatabaseCacheHelper helper;
 	protected RedmineConnection connection;
@@ -53,29 +36,7 @@ public class SelectTimeEntriesPost extends SelectDataTask<Void,RedmineTimeEntry>
 		SelectDataTaskConnectionHandler client = new SelectDataTaskRedmineConnectionHandler(connection);
 		RemoteUrlTimeEntries url = new RemoteUrlTimeEntries();
 		for(final RedmineTimeEntry item : params){
-			SelectDataTaskPutHandler puthandler = new SelectDataTaskPutHandler() {
-
-				@Override
-				public HttpEntity getContent() throws IOException,
-						SQLException, IllegalArgumentException, ParserConfigurationException, TransformerException {
-					DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
-					DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
-					Document document = dbuilder.newDocument();
-					Element root = item.getXml(document);
-					document.appendChild(root);
-
-					TransformerFactory transFactory = TransformerFactory.newInstance();
-					Transformer transformer = transFactory.newTransformer();
-					StringWriter writer = new StringWriter();
-					transformer.transform(new DOMSource(document), new StreamResult(writer));
-					String data = writer.toString();
-					StringEntity  entity = new StringEntity(data,"UTF-8");
-					entity.setContentType("application/xml");
-
-					if(BuildConfig.DEBUG) Log.d("post",entity.getContent().toString());
-					return entity;
-				}
-			};
+			SelectDataTaskPutHandler puthandler = getPutHandler(item);
 			if(item.getTimeentryId() == null){
 				url.setId(null);
 

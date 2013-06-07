1ac286353a3a0b3427f42149ebde484612be0451
in_dow <in_dow@hotmail.com>
2013-06-08 01:37:24 +0900

    Refactor ConnectionHelper

--- OpenRedmine/src/jp/redmine/redmineclient/task/SelectDataTaskRedmineConnectionHandler.java
+++ OpenRedmine/src/jp/redmine/redmineclient/task/SelectDataTaskRedmineConnectionHandler.java
@@ -3,10 +3,21 @@ package jp.redmine.redmineclient.task;
 import jp.redmine.redmineclient.entity.RedmineConnection;
 import jp.redmine.redmineclient.external.lib.AuthenticationParam;
 import jp.redmine.redmineclient.external.lib.ClientParam;
-import jp.redmine.redmineclient.external.lib.ConnectionHelper;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.scheme.SocketFactory;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.AbstractHttpMessage;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.transdroid.daemon.util.FakeSocketFactory;
 
 import android.net.Uri;
 import android.text.TextUtils;
@@ -17,12 +28,40 @@ class SelectDataTaskRedmineConnectionHandler extends SelectDataTaskConnectionHan
 		connection = con;
 	}
 
+	public static SchemeRegistry getSchemeRegistry(boolean isTrustAll, String certkey) {
+		SchemeRegistry registry = new SchemeRegistry();
+		registry.register(new Scheme("http", new PlainSocketFactory(), 80));
+		SocketFactory https_socket =
+				isTrustAll					? new FakeSocketFactory()
+			: !TextUtils.isEmpty(certkey)	? new FakeSocketFactory(certkey)
+			: SSLSocketFactory.getSocketFactory();
+		registry.register(new Scheme("https", https_socket, 443));
+		return registry;
+
+	}
+	public static DefaultHttpClient createHttpClient(ClientParam settings) {
+		HttpParams httpparams = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(httpparams, settings.getTimeout());
+		HttpConnectionParams.setSoTimeout(httpparams, settings.getTimeout());
+		DefaultHttpClient httpclient = new DefaultHttpClient(
+				new ThreadSafeClientConnManager(httpparams
+						, getSchemeRegistry(settings.isSLLTrustAll(),settings.getCertKey()))
+				, httpparams);
+
+		return httpclient;
+	}
+	public static void setupHttpClientAuthentication(DefaultHttpClient client, AuthenticationParam auth){
+		client.getCredentialsProvider().setCredentials(
+			new AuthScope(auth.getAddress(), auth.getPort()),
+			new UsernamePasswordCredentials(auth.getId(), auth.getPass()));
+	}
+
 	protected static DefaultHttpClient getHttpClient(RedmineConnection connection){
 		ClientParam clientparam = new ClientParam();
 		clientparam.setSLLTrustAll(connection.isPermitUnsafe());
 		clientparam.setCertKey(connection.getCertKey());
 		clientparam.setTimeout(120000);
-		DefaultHttpClient client = ConnectionHelper.createHttpClient(clientparam);
+		DefaultHttpClient client = createHttpClient(clientparam);
 		if(connection.isAuth()){
 			Uri remoteurl = Uri.parse(connection.getUrl());
 			AuthenticationParam param = new AuthenticationParam();
@@ -34,7 +73,7 @@ class SelectDataTaskRedmineConnectionHandler extends SelectDataTaskConnectionHan
 			} else {
 				param.setPort(remoteurl.getPort());
 			}
-			ConnectionHelper.setupHttpClientAuthentication(client, param);
+			setupHttpClientAuthentication(client, param);
 		}
 		return client;
 	}

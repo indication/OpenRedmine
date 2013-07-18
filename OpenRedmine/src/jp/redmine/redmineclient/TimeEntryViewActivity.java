f4fdd1b9037d3315ae8738c504d39c8746c21cd3
in_dow <in_dow@hotmail.com>
2013-07-18 23:47:26 +0900

    Add time entry list fragment

--- OpenRedmine/src/jp/redmine/redmineclient/TimeEntryViewActivity.java
+++ OpenRedmine/src/jp/redmine/redmineclient/TimeEntryViewActivity.java
@@ -2,74 +2,29 @@ package jp.redmine.redmineclient;
 
 import java.sql.SQLException;
 
-import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.android.apptools.OrmLiteFragmentActivity;
 
 import jp.redmine.redmineclient.activity.helper.ActivityHelper;
-import jp.redmine.redmineclient.adapter.RedmineTimeEntryListAdapter;
 import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
 import jp.redmine.redmineclient.db.cache.RedmineIssueModel;
 import jp.redmine.redmineclient.entity.RedmineIssue;
-import jp.redmine.redmineclient.entity.RedmineTimeEntry;
-import jp.redmine.redmineclient.form.RedmineBaseAdapterListFormHelper;
 import jp.redmine.redmineclient.form.RedmineIssueViewForm;
 import jp.redmine.redmineclient.intent.IssueIntent;
-import jp.redmine.redmineclient.intent.TimeEntryIntent;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
 
-public class TimeEntryViewActivity extends OrmLiteBaseActivity<DatabaseCacheHelper>  {
+public class TimeEntryViewActivity extends OrmLiteFragmentActivity<DatabaseCacheHelper>  {
 	public TimeEntryViewActivity(){
 		super();
 	}
 	private RedmineIssueViewForm form;
-	private RedmineBaseAdapterListFormHelper<RedmineTimeEntryListAdapter> formList;
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		formList.onSaveInstanceState(outState);
-		super.onSaveInstanceState(outState);
-	}
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		formList.onRestoreInstanceState(savedInstanceState);
-		super.onRestoreInstanceState(savedInstanceState);
-	}
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ActivityHelper.setupTheme(this);
 		setContentView(R.layout.timeentrylist);
-
-		formList = new RedmineBaseAdapterListFormHelper<RedmineTimeEntryListAdapter>();
-		formList.setList((ListView)findViewById(R.id.list));
-		formList.setAdapter(new RedmineTimeEntryListAdapter(getHelper()));
-		formList.onRestoreInstanceState(savedInstanceState);
-		formList.list.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long arg3) {
-				if(formList.adapter == null)
-					return;
-				@SuppressWarnings("deprecation")
-				RedmineTimeEntry entry = (RedmineTimeEntry)formList.adapter.getItem(position);
-				if(entry == null)
-					return;
-
-				TimeEntryIntent send = new TimeEntryIntent( getApplicationContext(), TimeEntryEditActivity.class );
-				send.setConnectionId(entry.getConnectionId());
-				send.setIssueId(entry.getIssueId());
-				send.setTimeEntryId(entry.getTimeentryId());
-				startActivity(send.getIntent());
-			}
-		});
 
 		form = new RedmineIssueViewForm(this);
 	}
@@ -86,7 +41,6 @@ public class TimeEntryViewActivity extends OrmLiteBaseActivity<DatabaseCacheHelp
 
 		RedmineIssueModel model = new RedmineIssueModel(getHelper());
 		RedmineIssue issue = new RedmineIssue();
-		Log.d("SelectDataTask","ParserIssue Start");
 		try {
 			issue = model.fetchById(connectionid, intent.getIssueId());
 		} catch (SQLException e) {
@@ -96,54 +50,6 @@ public class TimeEntryViewActivity extends OrmLiteBaseActivity<DatabaseCacheHelp
 			//item is not found
 		} else {
 			form.setValue(issue);
-
-			formList.adapter.setupParameter(connectionid,issue.getIssueId());
-			formList.refresh(isFetch);
-		}
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		super.onCreateOptionsMenu( menu );
-
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate( R.menu.timeentry_view, menu );
-		return true;
-	}
-	private final int FORM_TIMEENTRY = 1;
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch ( item.getItemId() )
-		{
-			case R.id.menu_refresh:
-			{
-				this.onRefresh(true);
-				return true;
-			}
-			case R.id.menu_access_addnew:
-			{
-				IssueIntent intent = new IssueIntent( getIntent() );
-				TimeEntryIntent send = new TimeEntryIntent( getApplicationContext(), TimeEntryEditActivity.class );
-				send.setConnectionId(intent.getConnectionId());
-				send.setIssueId(intent.getIssueId());
-				startActivityForResult(send.getIntent(),FORM_TIMEENTRY);
-				return true;
-			}
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		switch(requestCode){
-		case FORM_TIMEENTRY:
-			if(resultCode !=RESULT_OK )
-				break;
-			finish();
-			break;
-		default:
-			break;
 		}
 	}
 
---
+++ OpenRedmine/src/jp/redmine/redmineclient/fragment/TimeEntryList.java
@@ -0,0 +1,129 @@
+package jp.redmine.redmineclient.fragment;
+
+import com.j256.ormlite.android.apptools.OrmLiteListFragment;
+
+import jp.redmine.redmineclient.R;
+import jp.redmine.redmineclient.TimeEntryEditActivity;
+import jp.redmine.redmineclient.adapter.RedmineTimeEntryListAdapter;
+import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
+import jp.redmine.redmineclient.entity.RedmineTimeEntry;
+import jp.redmine.redmineclient.intent.IssueIntent;
+import jp.redmine.redmineclient.intent.TimeEntryIntent;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class TimeEntryList extends OrmLiteListFragment<DatabaseCacheHelper> {
+	private RedmineTimeEntryListAdapter adapter;
+	private View mFooter;
+
+	private final int FORM_TIMEENTRY = 1;
+
+	public TimeEntryList(){
+		super();
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		getListView().addFooterView(mFooter);
+
+		adapter = new RedmineTimeEntryListAdapter(getHelper());
+		setListAdapter(adapter);
+
+		getListView().setFastScrollEnabled(true);
+
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		mFooter = inflater.inflate(R.layout.listview_footer,null);
+		mFooter.setVisibility(View.GONE);
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		onRefresh(true);
+	}
+
+	@Override
+	public void onListItemClick(ListView parent, View v, int position, long id) {
+		super.onListItemClick(parent, v, position, id);
+		ListView listView = (ListView) parent;
+		Object item =  listView.getItemAtPosition(position);
+		if(item == null || !(item instanceof RedmineTimeEntry))
+			return;
+		onItemSelect((RedmineTimeEntry)item);
+	}
+
+	protected void onItemSelect(RedmineTimeEntry entry) {
+		TimeEntryIntent send = new TimeEntryIntent( getActivity().getApplicationContext(), TimeEntryEditActivity.class );
+		send.setConnectionId(entry.getConnectionId());
+		send.setIssueId(entry.getIssueId());
+		send.setTimeEntryId(entry.getTimeentryId());
+		startActivity(send.getIntent());
+	}
+
+	protected void onRefresh(boolean isFetch){
+		IssueIntent intent = new IssueIntent(getActivity().getIntent());
+		int connectionid = intent.getConnectionId();
+
+		adapter.setupParameter(connectionid,intent.getIssueId());
+		adapter.notifyDataSetInvalidated();
+		adapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate( R.menu.timeentry_view, menu );
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch ( item.getItemId() )
+		{
+			case R.id.menu_refresh:
+			{
+				this.onRefresh(true);
+				return true;
+			}
+			case R.id.menu_access_addnew:
+			{
+				IssueIntent intent = new IssueIntent( getActivity().getIntent() );
+				TimeEntryIntent send = new TimeEntryIntent( getActivity().getApplicationContext(), TimeEntryEditActivity.class );
+				send.setConnectionId(intent.getConnectionId());
+				send.setIssueId(intent.getIssueId());
+				startActivityForResult(send.getIntent(),FORM_TIMEENTRY);
+				return true;
+			}
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch(requestCode){
+		case FORM_TIMEENTRY:
+			if(resultCode != Activity.RESULT_OK )
+				break;
+			getActivity().finish();
+			break;
+		default:
+			break;
+		}
+	}
+}

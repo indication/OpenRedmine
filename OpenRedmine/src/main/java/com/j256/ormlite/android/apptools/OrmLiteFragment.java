ed0e230794bbe5affa2dbf2f059b9e5595ae1690
in_dow <in_dow@hotmail.com>
2013-11-20 01:53:25 +0900

    Reactor ormlite base classes

--- OpenRedmine/src/main/java/com/j256/ormlite/android/apptools/OrmLiteFragment.java
+++ OpenRedmine/src/main/java/com/j256/ormlite/android/apptools/OrmLiteFragment.java
@@ -7,6 +7,7 @@ import com.j256.ormlite.support.ConnectionSource;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Base class to use for fragment in Android.
@@ -22,87 +23,28 @@ import android.support.v4.app.Fragment;
  * @author graywatson, kevingalligan
  */
 public abstract class OrmLiteFragment<H extends OrmLiteSqliteOpenHelper> extends Fragment {
-
-	private volatile H helper;
-	private volatile boolean created = false;
-	private volatile boolean destroyed = false;
 	private static Logger logger = LoggerFactory.getLogger(OrmLiteBaseActivity.class);
 
 	/**
 	 * Get a helper for this action.
 	 */
 	public H getHelper() {
-		if (helper == null) {
-			if (!created) {
-				throw new IllegalStateException("A call has not been made to onCreate() yet so the helper is null");
-			} else if (destroyed) {
-				throw new IllegalStateException(
-						"A call to onDestroy has already been made and the helper cannot be used after that point");
-			} else {
-				throw new IllegalStateException("Helper is null for some unknown reason");
-			}
-		} else {
-			return helper;
-		}
+		if(getActivity() instanceof OrmLiteFragmentActivity<?>){
+            return ((OrmLiteFragmentActivity<H>)getActivity()).getHelper();
+        } else {
+            return null;
+        }
 	}
 
-	/**
-	 * Get a connection source for this action.
-	 */
-	public ConnectionSource getConnectionSource() {
-		return getHelper().getConnectionSource();
-	}
+    /**
+     * Get a connection source for this action.
+     */
+    public ConnectionSource getConnectionSource() {
+        return getHelper().getConnectionSource();
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		if (helper == null) {
-			helper = getHelperInternal(this.getActivity());
-			created = true;
-		}
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		releaseHelper(helper);
-		destroyed = true;
-	}
-
-	/**
-	 * This is called internally by the class to populate the helper object instance. This should not be called directly
-	 * by client code unless you know what you are doing. Use {@link #getHelper()} to get a helper instance. If you are
-	 * managing your own helper creation, override this method to supply this activity with a helper instance.
-	 *
-	 * <p>
-	 * <b> NOTE: </b> If you override this method, you most likely will need to override the
-	 * {@link #releaseHelper(OrmLiteSqliteOpenHelper)} method as well.
-	 * </p>
-	 */
-	protected H getHelperInternal(Context context) {
-		@SuppressWarnings({ "unchecked", "deprecation" })
-		H newHelper = (H) OpenHelperManager.getHelper(context);
-		logger.trace("{}: got new helper {} from OpenHelperManager", this, newHelper);
-		return newHelper;
-	}
-
-	/**
-	 * Release the helper instance created in {@link #getHelperInternal(Context)}. You most likely will not need to call
-	 * this directly since {@link #onDestroy()} does it for you.
-	 *
-	 * <p>
-	 * <b> NOTE: </b> If you override this method, you most likely will need to override the
-	 * {@link #getHelperInternal(Context)} method as well.
-	 * </p>
-	 */
-	protected void releaseHelper(H helper) {
-		OpenHelperManager.releaseHelper();
-		logger.trace("{}: helper {} was released, set to null", this, helper);
-		this.helper = null;
-	}
-
-	@Override
-	public String toString() {
-		return getClass().getSimpleName() + "@" + Integer.toHexString(super.hashCode());
-	}
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "@" + Integer.toHexString(super.hashCode());
+    }
 }
--- OpenRedmine/src/main/java/com/j256/ormlite/android/apptools/OrmLiteListFragment.java
+++ OpenRedmine/src/main/java/com/j256/ormlite/android/apptools/OrmLiteListFragment.java
@@ -23,83 +23,23 @@ import android.support.v4.app.ListFragment;
  */
 public abstract class OrmLiteListFragment<H extends OrmLiteSqliteOpenHelper> extends ListFragment {
 
-	private volatile H helper;
-	private volatile boolean created = false;
-	private volatile boolean destroyed = false;
-	private static Logger logger = LoggerFactory.getLogger(OrmLiteListFragment.class);
+    /**
+     * Get a helper for this action.
+     */
+    public H getHelper() {
+        if(getActivity() instanceof OrmLiteFragmentActivity<?>){
+            return ((OrmLiteFragmentActivity<H>)getActivity()).getHelper();
+        } else {
+            return null;
+        }
+    }
 
-	/**
-	 * Get a helper for this action.
-	 */
-	public H getHelper() {
-		if (helper == null) {
-			if (!created) {
-				throw new IllegalStateException("A call has not been made to onCreate() yet so the helper is null");
-			} else if (destroyed) {
-				throw new IllegalStateException(
-						"A call to onDestroy has already been made and the helper cannot be used after that point");
-			} else {
-				throw new IllegalStateException("Helper is null for some unknown reason");
-			}
-		} else {
-			return helper;
-		}
-	}
-
-	/**
-	 * Get a connection source for this action.
-	 */
-	public ConnectionSource getConnectionSource() {
-		return getHelper().getConnectionSource();
-	}
-
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		if (helper == null) {
-			helper = getHelperInternal(this.getActivity());
-			created = true;
-		}
-	}
-
-	@Override
-	public void onDestroyView() {
-		super.onDestroyView();
-		releaseHelper(helper);
-		destroyed = true;
-	}
-
-	/**
-	 * This is called internally by the class to populate the helper object instance. This should not be called directly
-	 * by client code unless you know what you are doing. Use {@link #getHelper()} to get a helper instance. If you are
-	 * managing your own helper creation, override this method to supply this activity with a helper instance.
-	 *
-	 * <p>
-	 * <b> NOTE: </b> If you override this method, you most likely will need to override the
-	 * {@link #releaseHelper(OrmLiteSqliteOpenHelper)} method as well.
-	 * </p>
-	 */
-	protected H getHelperInternal(Context context) {
-		@SuppressWarnings({ "unchecked", "deprecation" })
-		H newHelper = (H) OpenHelperManager.getHelper(context);
-		logger.trace("{}: got new helper {} from OpenHelperManager", this, newHelper);
-		return newHelper;
-	}
-
-	/**
-	 * Release the helper instance created in {@link #getHelperInternal(Context)}. You most likely will not need to call
-	 * this directly since {@link #onDestroy()} does it for you.
-	 *
-	 * <p>
-	 * <b> NOTE: </b> If you override this method, you most likely will need to override the
-	 * {@link #getHelperInternal(Context)} method as well.
-	 * </p>
-	 */
-	protected void releaseHelper(H helper) {
-		OpenHelperManager.releaseHelper();
-		logger.trace("{}: helper {} was released, set to null", this, helper);
-		this.helper = null;
-	}
+    /**
+     * Get a connection source for this action.
+     */
+    public ConnectionSource getConnectionSource() {
+        return getHelper().getConnectionSource();
+    }
 
 	@Override
 	public String toString() {

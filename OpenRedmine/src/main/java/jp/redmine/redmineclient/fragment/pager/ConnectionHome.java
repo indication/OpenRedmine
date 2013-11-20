f701f6cf613290cf7a7121ce58f302808973be23
in_dow <in_dow@hotmail.com>
2013-11-21 02:51:28 +0900

    Refactor ConnectionPager to support i11n

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/pager/ConnectionHome.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/pager/ConnectionHome.java
@@ -10,11 +10,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.j256.ormlite.android.apptools.OrmLiteFragment;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import jp.redmine.redmineclient.R;
+import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
+import jp.redmine.redmineclient.fragment.IssueJump;
+import jp.redmine.redmineclient.fragment.ProjectList;
 import jp.redmine.redmineclient.param.ConnectionArgument;
 import jp.redmine.redmineclient.param.ProjectArgument;
 
-public class ConnectionHome extends Fragment {
+public class ConnectionHome extends OrmLiteFragment<DatabaseCacheHelper> {
 	private static final String TAG = ConnectionHome.class.getSimpleName();
 
 	public ConnectionHome() {
@@ -30,10 +38,38 @@ public class ConnectionHome extends Fragment {
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		ProjectArgument arg = new ProjectArgument();
-		arg.setArgument(getArguments());
+		List<CorePager.PageFragment> list = new LinkedList<CorePager.PageFragment>();
+		// Project list
+		list.add(new CorePager.PageFragment() {
+			@Override
+			public Fragment getFragment() {
+				ConnectionArgument arg = new ConnectionArgument();
+				arg.setArgument(getArguments());
+				return ProjectList.newInstance(arg);
+			}
+
+			@Override
+			public CharSequence getName() {
+				return getActivity().getString(R.string.ticket_project);
+			}
+		});
+		// Direct issue jump list
+		list.add(new CorePager.PageFragment() {
+			@Override
+			public Fragment getFragment() {
+				ConnectionArgument arg = new ConnectionArgument();
+				arg.setArgument(getArguments());
+				return IssueJump.newInstance(arg);
+			}
+
+			@Override
+			public CharSequence getName() {
+				return getActivity().getString(R.string.ticket_jump);
+			}
+		});
+
 		ViewPager viewPager = (ViewPager) getView().findViewById(R.id.pager);
-		viewPager.setAdapter(new ConnectionPager(getChildFragmentManager(), arg));
+		viewPager.setAdapter(new CorePager(getChildFragmentManager(), list));
 	}
 
 	@Override

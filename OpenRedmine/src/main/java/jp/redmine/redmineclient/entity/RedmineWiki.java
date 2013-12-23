b526847967aa32d1df5e70e7a61cfe6cadd89985
in_dow <in_dow@hotmail.com>
2013-12-24 00:55:12 +0900

    Add wiki schemas

---
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/entity/RedmineWiki.java
@@ -0,0 +1,170 @@
+package jp.redmine.redmineclient.entity;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+import java.util.List;
+
+@DatabaseTable
+public class RedmineWiki {
+	public final static String ID = "id";
+	public final static String CONNECTION = "connection_id";
+	public final static String PROJECT_ID = "project_id";
+	public final static String TITLE = "title";
+	public final static String NAME = "name";
+
+	@DatabaseField(generatedId = true)
+	private Long id;
+	@DatabaseField(uniqueIndexName = "wiki_target")
+	private Integer connection_id;
+	@DatabaseField(uniqueIndexName = "wiki_target", foreign = true, foreignColumnName = "id", columnName = "project_id", foreignAutoRefresh = true)
+	private RedmineProject project;
+	@DatabaseField(uniqueIndexName = "wiki_target")
+	private String title;
+	@DatabaseField
+	private int version;
+	@DatabaseField
+	private String body;
+	@DatabaseField
+	private String body_html;
+	@DatabaseField
+	private String parent;
+	@DatabaseField(foreign = true,foreignColumnName="id", columnName= "author_id", foreignAutoRefresh = true)
+	private RedmineUser author;
+	@DatabaseField
+	private String comment;
+	@DatabaseField
+	private Date created;
+	@DatabaseField
+	private Date modified;
+	//supports for nested sort
+	@DatabaseField
+	private int rgt;
+	@DatabaseField
+	private int lft;
+
+	public Date getData_modified() {
+		return data_modified;
+	}
+
+	public void setData_modified(Date data_modified) {
+		this.data_modified = data_modified;
+	}
+
+	public List<RedmineAttachment> getAttachments() {
+		return attachments;
+	}
+
+	public void setAttachments(List<RedmineAttachment> attachments) {
+		this.attachments = attachments;
+	}
+
+	@DatabaseField
+	private Date data_modified;
+
+	private List<RedmineAttachment> attachments;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setRedmineConnection(RedmineConnection connection) {
+		this.setConnectionId(connection.getId());
+	}
+
+	public RedmineProject getProject() {
+		return project;
+	}
+
+	public void setProject(RedmineProject project) {
+		this.project = project;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getBodyHtml() {
+		return body_html;
+	}
+
+	public void setBodyHtml(String body_html) {
+		this.body_html = body_html;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getParent() {
+		return parent;
+	}
+
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+
+	public RedmineUser getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(RedmineUser author) {
+		this.author = author;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setModified(Date modified) {
+		this.modified = modified;
+	}
+
+	public Date getModified() {
+		return modified;
+	}
+
+
+	public void setConnectionId(Integer connection_id) {
+		this.connection_id = connection_id;
+	}
+
+	public Integer getConnectionId() {
+		return connection_id;
+	}
+
+
+}
---
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/parser/ParserWiki.java
@@ -0,0 +1,49 @@
+package jp.redmine.redmineclient.parser;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import jp.redmine.redmineclient.entity.RedmineProject;
+import jp.redmine.redmineclient.entity.RedmineUser;
+import jp.redmine.redmineclient.entity.RedmineWiki;
+import jp.redmine.redmineclient.entity.TypeConverter;
+
+public class ParserWiki extends BaseParserInternal<RedmineProject,RedmineWiki> {
+
+	@Override
+	protected String getProveTagName() {
+		return "wiki_page";
+	}
+
+	@Override
+	protected RedmineWiki getNewProveTagItem() {
+		return new RedmineWiki();
+	}
+
+	@Override
+	protected void parseInternal(RedmineProject con, RedmineWiki item)
+			throws XmlPullParserException, IOException, SQLException {
+		if(xml.getDepth() <= 2)
+			return;
+		if(equalsTagName("title")){
+			item.setTitle(getNextText());
+		} else if(equalsTagName("version")){
+			item.setVersion(getTextInteger());
+		} else if(equalsTagName("text")){
+			item.setBody(getNextText());
+		} else if(equalsTagName("author")){
+			RedmineUser tk = new RedmineUser();
+			setMasterRecord(tk);
+			item.setAuthor(tk);
+		} else if(equalsTagName("parent")){
+			item.setParent(getAttributeString("title"));
+		} else if(equalsTagName("created_on")){
+			item.setCreated(TypeConverter.parseDateTime(getNextText()));
+		} else if(equalsTagName("updated_on")){
+			item.setModified(TypeConverter.parseDateTime(getNextText()));
+		}
+
+	}
+}

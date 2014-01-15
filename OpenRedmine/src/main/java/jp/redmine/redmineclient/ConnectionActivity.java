5763c0e50fed6fc2a38066a2be976dfec5737166
in_dow <in_dow@hotmail.com>
2014-01-16 01:10:02 +0900

    Refactor connection list

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/ConnectionActivity.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/ConnectionActivity.java
@@ -1,86 +1,84 @@
 package jp.redmine.redmineclient;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 
-import com.actionbarsherlock.app.ActionBar;
-import com.j256.ormlite.android.apptools.OrmLiteFragmentActivity;
-
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import jp.redmine.redmineclient.activity.handler.AttachmentActionHandler;
-import jp.redmine.redmineclient.activity.handler.AttachmentActionInterface;
-import jp.redmine.redmineclient.activity.handler.ConnectionActionInterface;
-import jp.redmine.redmineclient.activity.handler.ConnectionListHandler;
-import jp.redmine.redmineclient.activity.handler.Core.ActivityRegistry;
-import jp.redmine.redmineclient.activity.handler.IssueActionInterface;
-import jp.redmine.redmineclient.activity.handler.IssueViewHandler;
-import jp.redmine.redmineclient.activity.helper.TabHelper;
-import jp.redmine.redmineclient.activity.handler.TimeEntryHandler;
-import jp.redmine.redmineclient.activity.handler.TimeentryActionInterface;
-import jp.redmine.redmineclient.activity.handler.WebviewActionInterface;
-import jp.redmine.redmineclient.activity.helper.ActivityHelper;
+import jp.redmine.redmineclient.activity.TabActivity;
+import jp.redmine.redmineclient.activity.pager.CorePage;
 import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
 import jp.redmine.redmineclient.db.cache.RedmineFilterModel;
 import jp.redmine.redmineclient.db.cache.RedmineUserModel;
 import jp.redmine.redmineclient.entity.RedmineFilter;
 import jp.redmine.redmineclient.entity.RedmineFilterSortItem;
 import jp.redmine.redmineclient.entity.RedmineUser;
-import jp.redmine.redmineclient.fragment.ActivityInterface;
 import jp.redmine.redmineclient.fragment.IssueJump;
 import jp.redmine.redmineclient.fragment.IssueList;
 import jp.redmine.redmineclient.fragment.ProjectList;
 import jp.redmine.redmineclient.param.ConnectionArgument;
 import jp.redmine.redmineclient.param.FilterArgument;
 
-public class ConnectionActivity extends OrmLiteFragmentActivity<DatabaseCacheHelper>
-	implements ActivityInterface {
+public class ConnectionActivity extends TabActivity<DatabaseCacheHelper> {
 	private static final String TAG = ConnectionActivity.class.getSimpleName();
 	public ConnectionActivity(){
 		super();
 	}
-
-	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		ActivityHelper.setupTheme(this);
-		super.onCreate(savedInstanceState);
-		getSupportActionBar();
-		setContentView(R.layout.fragment_one);
-
-		int target_layout = R.id.fragmentOne;
-		ActionBar actionBar = getSupportActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-		actionBar.setDisplayShowTitleEnabled(true);
+	protected List<CorePage> getTabs(){
 
 		ConnectionArgument intent = new ConnectionArgument();
 		intent.setIntent(getIntent());
 
+		ConnectionArgument arg = new ConnectionArgument();
+		arg.setArgument();
+		arg.setConnectionId(intent.getConnectionId());
+
+
+		List<CorePage> list = new ArrayList<CorePage>();
 		// Project list
-		ConnectionArgument argProject = new ConnectionArgument();
-		argProject.setArgument();
-		argProject.setConnectionId(intent.getConnectionId());
+		ConnectionArgument argList = new ConnectionArgument();
+		argList.setArgument(arg.getArgument(), true);
+		list.add((new CorePage<ConnectionArgument>() {
+			@Override
+			public Fragment getRawFragment() {
+				return ProjectList.newInstance(getParam());
+			}
 
-		actionBar.addTab(actionBar.newTab()
-				.setText(R.string.ticket_project)
-				.setTabListener(new TabHelper(ProjectList.newInstance(argProject), target_layout))
-				.setIcon(android.R.drawable.ic_menu_mapmode)
-			);
+			@Override
+			public CharSequence getName() {
+				return getString(R.string.ticket_project);
+			}
 
+			@Override
+			public Integer getIcon() {
+				return android.R.drawable.ic_menu_mapmode;
+			}
+		}).setParam(argList));
 
 		// Direct issue jump list
 		ConnectionArgument argJump = new ConnectionArgument();
-		argJump.setArgument();
-		argJump.setConnectionId(intent.getConnectionId());
+		argJump.setArgument(arg.getArgument(), true);
+		list.add((new CorePage<ConnectionArgument>() {
+			@Override
+			public Fragment getRawFragment() {
+				return IssueJump.newInstance(getParam());
+			}
 
-		actionBar.addTab(actionBar.newTab()
-				.setText(R.string.ticket_jump)
-				.setTabListener(new TabHelper(IssueJump.newInstance(argJump), target_layout))
-				.setIcon(android.R.drawable.ic_menu_directions)
-		);
+			@Override
+			public CharSequence getName() {
+				return getString(R.string.ticket_jump);
+			}
+
+			@Override
+			public Integer getIcon() {
+				return android.R.drawable.ic_menu_directions;
+			}
+		}).setParam(argJump));
+
+
 
 		RedmineUserModel mUserModel = new RedmineUserModel(getHelper());
 		try {
@@ -98,52 +96,30 @@ public class ConnectionActivity extends OrmLiteFragmentActivity<DatabaseCacheHel
 				}
 
 				FilterArgument argIssue = new FilterArgument();
-				argIssue.setArgument();
-				argIssue.setConnectionId(intent.getConnectionId());
+				argIssue.setArgument(arg.getArgument(), true);
 				argIssue.setFilterId(target.getId());
-				actionBar.addTab(actionBar.newTab()
-						.setText(user.getName())
-						.setTabListener(new TabHelper(IssueList.newInstance(argIssue), target_layout))
-						.setIcon(R.drawable.ic_action_user)
-				);
+				list.add((new CorePage<FilterArgument>() {
+					@Override
+					public Fragment getRawFragment() {
+						return IssueList.newInstance(getParam());
+					}
+
+					@Override
+					public CharSequence getName() {
+						return user.getName();
+					}
+
+					@Override
+					public Integer getIcon() {
+						return R.drawable.ic_action_user;
+					}
+				}).setParam(argIssue));
 			}
 		} catch (SQLException e) {
 			Log.e(TAG,"fetchCurrentUser", e);
 		}
 
-	}
-
-	@SuppressWarnings("unchecked")
-	public <T> T getHandler(Class<T> cls){
-		ActivityRegistry registry = new ActivityRegistry(){
-
-			@Override
-			public FragmentManager getFragment() {
-				return getSupportFragmentManager();
-			}
-
-			@Override
-			public Intent getIntent(Class<?> activity) {
-				return new Intent(getApplicationContext(),activity);
-			}
-
-			@Override
-			public void kickActivity(Intent intent) {
-				startActivity(intent);
-			}
-
-		};
-		if(cls.equals(ConnectionActionInterface.class))
-			return (T) new ConnectionListHandler(registry);
-		if(cls.equals(WebviewActionInterface.class))
-			return (T) new IssueViewHandler(registry);
-		if(cls.equals(IssueActionInterface.class))
-			return (T) new IssueViewHandler(registry);
-		if(cls.equals(TimeentryActionInterface.class))
-			return (T) new TimeEntryHandler(registry);
-		if(cls.equals(AttachmentActionInterface.class))
-			return (T) new AttachmentActionHandler(registry);
-		return null;
+		return list;
 	}
 
 

4f020d2e028d94f74e4c8474bc25aa057e19e3c5
in_dow <in_dow@hotmail.com>
2014-01-20 02:32:31 +0900

    Fix navigation to project list

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/IssueActivity.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/IssueActivity.java
@@ -1,20 +1,28 @@
 package jp.redmine.redmineclient;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.MenuItem;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import jp.redmine.redmineclient.activity.TabActivity;
+import jp.redmine.redmineclient.activity.handler.IssueActionInterface;
+import jp.redmine.redmineclient.activity.handler.IssueViewHandler;
 import jp.redmine.redmineclient.activity.pager.CorePage;
 import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
 import jp.redmine.redmineclient.db.cache.RedmineFilterModel;
+import jp.redmine.redmineclient.db.cache.RedmineIssueModel;
 import jp.redmine.redmineclient.db.cache.RedmineProjectModel;
 import jp.redmine.redmineclient.db.cache.RedmineUserModel;
 import jp.redmine.redmineclient.entity.RedmineFilter;
 import jp.redmine.redmineclient.entity.RedmineFilterSortItem;
+import jp.redmine.redmineclient.entity.RedmineIssue;
 import jp.redmine.redmineclient.entity.RedmineProject;
 import jp.redmine.redmineclient.entity.RedmineUser;
 import jp.redmine.redmineclient.fragment.ActivityInterface;
@@ -35,6 +43,13 @@ public class IssueActivity extends TabActivity<DatabaseCacheHelper>
 	}
 
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
 	protected List<CorePage> getTabs(){
 
 		IssueArgument intent = new IssueArgument();
@@ -45,6 +60,26 @@ public class IssueActivity extends TabActivity<DatabaseCacheHelper>
 		arg.setConnectionId(intent.getConnectionId());
 		arg.setProjectId(intent.getProjectId());
 		arg.setIssueId(intent.getIssueId());
+
+		// setup navigation
+		try {
+			RedmineProject proj = null;
+			if(intent.getProjectId() < 0){
+				RedmineIssueModel mIssue = new RedmineIssueModel(getHelper());
+				RedmineIssue issue = mIssue.fetchById(intent.getConnectionId(), intent.getIssueId());
+				proj = issue.getProject();
+			}
+			if(proj == null){
+				RedmineProjectModel mProject = new RedmineProjectModel(getHelper());
+				proj = mProject.fetchById(intent.getProjectId());
+			}
+			if(proj != null && proj.getId() != null){
+				intent.setProjectId(proj.getId());
+				setTitle(proj.getName());
+			}
+		} catch (SQLException e) {
+			Log.e(TAG, "getTabs", e);
+		}
 
 		List<CorePage> list = new ArrayList<CorePage>();
 		// Issue view
@@ -70,4 +105,20 @@ public class IssueActivity extends TabActivity<DatabaseCacheHelper>
 		return list;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+
+				IssueArgument intent = new IssueArgument();
+				intent.setIntent(getIntent());
+				if(intent.getProjectId() > 0){
+					IssueActionInterface handler = getHandler(IssueActionInterface.class);
+					handler.onIssueList(intent.getConnectionId(), intent.getProjectId());
+				}
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }

64067b1c5d777deb3d360f3542e576143f29b645
in_dow <in_dow@hotmail.com>
2014-02-13 01:25:28 +0900

    Add favorite project list fix #50
    Tryed to implement with adapter that has dao internal
    refs #61
    
    Referenced:
    https://groups.google.com/forum/#!topic/ormlite-android/5cShoCPkga4

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/ConnectionListActivity.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/ConnectionListActivity.java
@@ -1,79 +1,59 @@
 package jp.redmine.redmineclient;
 
-import jp.redmine.redmineclient.activity.handler.AttachmentActionHandler;
-import jp.redmine.redmineclient.activity.handler.AttachmentActionInterface;
-import jp.redmine.redmineclient.activity.handler.ConnectionActionInterface;
-import jp.redmine.redmineclient.activity.handler.ConnectionListHandler;
-import jp.redmine.redmineclient.activity.handler.Core.ActivityRegistry;
-import jp.redmine.redmineclient.activity.handler.IssueActionInterface;
-import jp.redmine.redmineclient.activity.handler.IssueViewHandler;
-import jp.redmine.redmineclient.activity.handler.TimeEntryHandler;
-import jp.redmine.redmineclient.activity.handler.TimeentryActionInterface;
-import jp.redmine.redmineclient.activity.handler.WebviewActionInterface;
-import jp.redmine.redmineclient.activity.helper.ActivityHelper;
+import jp.redmine.redmineclient.activity.TabActivity;
+import jp.redmine.redmineclient.activity.pager.CorePage;
 import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
 import jp.redmine.redmineclient.fragment.ActivityInterface;
 import jp.redmine.redmineclient.fragment.ConnectionList;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import jp.redmine.redmineclient.fragment.ProjectFavoriteList;
 
-import com.j256.ormlite.android.apptools.OrmLiteFragmentActivity;
+import android.support.v4.app.Fragment;
 
-public class ConnectionListActivity extends OrmLiteFragmentActivity<DatabaseCacheHelper>
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionListActivity extends TabActivity<DatabaseCacheHelper>
 	implements ActivityInterface {
 	public ConnectionListActivity(){
 		super();
 	}
-
-	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		ActivityHelper.setupTheme(this);
-		super.onCreate(savedInstanceState);
-		getSupportActionBar();
+	protected List<CorePage> getTabs(){
 
-		/**
-		 * Add fragment on first view only
-		 * On rotate, this method would be called with savedInstanceState.
-		 */
-		if(savedInstanceState != null)
-			return;
-		getSupportFragmentManager().beginTransaction()
-			.replace(android.R.id.content, ConnectionList.newInstance())
-			.commit();
-	}
-
-	@SuppressWarnings("unchecked")
-	public <T> T getHandler(Class<T> cls){
-		ActivityRegistry registry = new ActivityRegistry(){
-
+		List<CorePage> list = new ArrayList<CorePage>();
+		list.add((new CorePage<Void>() {
 			@Override
-			public FragmentManager getFragment() {
-				return getSupportFragmentManager();
+			public Fragment getRawFragment() {
+				return ConnectionList.newInstance();
 			}
 
 			@Override
-			public Intent getIntent(Class<?> activity) {
-				return new Intent(getApplicationContext(),activity);
+			public CharSequence getName() {
+				return getString(R.string.connection);
 			}
 
 			@Override
-			public void kickActivity(Intent intent) {
-				startActivity(intent);
+			public Integer getIcon() {
+				return android.R.drawable.ic_menu_mapmode;
+			}
+		}).setParam(null));
+
+		list.add((new CorePage<Void>() {
+			@Override
+			public Fragment getRawFragment() {
+				return ProjectFavoriteList.newInstance();
 			}
 
-		};
-		if(cls.equals(ConnectionActionInterface.class))
-			return (T) new ConnectionListHandler(registry);
-		if(cls.equals(WebviewActionInterface.class))
-			return (T) new IssueViewHandler(registry);
-		if(cls.equals(IssueActionInterface.class))
-			return (T) new IssueViewHandler(registry);
-		if(cls.equals(TimeentryActionInterface.class))
-			return (T) new TimeEntryHandler(registry);
-		if(cls.equals(AttachmentActionInterface.class))
-			return (T) new AttachmentActionHandler(registry);
-		return null;
+			@Override
+			public CharSequence getName() {
+				return getString(R.string.favorite);
+			}
+
+			@Override
+			public Integer getIcon() {
+				return android.R.drawable.btn_star;
+			}
+		}).setParam(null));
+		return list;
 	}
 }

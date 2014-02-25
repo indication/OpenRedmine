b3412a83e3ecf22f5f138306327972c406a38cbe
in_dow <in_dow@hotmail.com>
2014-02-26 01:29:51 +0900

    Support base class to return Filter

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/adapter/RedmineDaoAdapter.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/adapter/RedmineDaoAdapter.java
@@ -1,22 +1,29 @@
 package jp.redmine.redmineclient.adapter;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import com.j256.ormlite.android.AndroidDatabaseResults;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.support.DatabaseResults;
 
 import java.sql.SQLException;
 
 import jp.redmine.redmineclient.R;
 
-public abstract class RedmineDaoAdapter<T, ID, H extends OrmLiteSqliteOpenHelper> extends BaseAdapter {
+public abstract class RedmineDaoAdapter<T, ID, H extends OrmLiteSqliteOpenHelper>
+		extends BaseAdapter
+		implements Filterable
+{
 	private static final String TAG = RedmineDaoAdapter.class.getSimpleName();
 	protected final LayoutInflater infrator;
 	protected Dao<T, ID> dao;
@@ -25,6 +32,9 @@ public abstract class RedmineDaoAdapter<T, ID, H extends OrmLiteSqliteOpenHelper
 	protected abstract int getItemViewId();
 	protected abstract void setupView(View view,T data);
 	protected abstract QueryBuilder<T, ID> getQueryBuilder() throws SQLException;
+	protected QueryBuilder<T, ID> getSearchQueryBuilder(String search) throws SQLException{
+		return null;
+	}
 
 	public RedmineDaoAdapter(H helper, Context context, Class<T> daoClass){
 		super();
@@ -50,6 +60,9 @@ public abstract class RedmineDaoAdapter<T, ID, H extends OrmLiteSqliteOpenHelper
 
             }
         }
+		super.notifyDataSetChanged();
+	}
+	private void notifySuperDataSetChanged(){
 		super.notifyDataSetChanged();
 	}
 	@Override
@@ -141,4 +154,44 @@ public abstract class RedmineDaoAdapter<T, ID, H extends OrmLiteSqliteOpenHelper
     public boolean isValidParameter(){
         return true;
     }
+
+	@Override
+	public Filter getFilter() {
+		return new Filter() {
+			@Override
+			protected FilterResults performFiltering(CharSequence charSequence) {
+				FilterResults result = new FilterResults();
+				if(isValidParameter()){
+					try {
+						QueryBuilder<T, ID> query;
+						if(TextUtils.isEmpty(charSequence)){
+							query = getQueryBuilder();
+						} else {
+							String str = charSequence.toString().toLowerCase().trim();
+							query = getSearchQueryBuilder(str);
+						}
+						if(query == null)
+							return result;
+						AndroidDatabaseResults ret = (AndroidDatabaseResults)dao.iterator(query.prepare()).getRawResults();
+						result.values =  ret;
+						result.count = ret.getCount();
+					} catch (SQLException e) {
+						Log.e(TAG,"performFiltering" , e);
+					}
+				}
+				return result;
+			}
+
+			@Override
+			protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+				if(filterResults.count == 0){
+					notifyDataSetInvalidated();
+				} else {
+					close();
+					dbResults = (AndroidDatabaseResults) filterResults.values;
+					notifySuperDataSetChanged();
+				}
+			}
+		};
+	}
 }

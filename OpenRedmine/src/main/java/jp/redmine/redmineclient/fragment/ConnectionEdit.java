deae15b08bebcb58d77ec681ae83ee3a6af5a8ed
in_dow <in_dow@hotmail.com>
2014-07-23 01:22:37 +0900

    Rename connection

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/ConnectionEdit.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/ConnectionEdit.java
@@ -53,7 +53,7 @@ public class ConnectionEdit extends OrmLiteFragment<DatabaseCacheHelper> {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.connection, container, false);
+		return inflater.inflate(R.layout.input_connection, container, false);
 	}
 
 	@Override

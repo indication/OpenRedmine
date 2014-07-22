169797419441f2525c7905563d630eb87e210890
in_dow <in_dow@hotmail.com>
2014-07-23 01:27:44 +0900

    Rename downloaditem

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/FileDownload.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/FileDownload.java
@@ -46,7 +46,7 @@ public class FileDownload extends OrmLiteFragment<DatabaseCacheHelper> {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.downloaditem, container, false);
+		return inflater.inflate(R.layout.page_download, container, false);
 	}
 
 	@Override

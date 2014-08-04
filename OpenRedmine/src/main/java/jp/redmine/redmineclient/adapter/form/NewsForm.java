570cf3e7cc9246f07fea02f14d3ab8fff907a9c1
in_dow <in_dow@hotmail.com>
2014-08-05 01:05:17 +0900

    Switch from webview to textview

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/adapter/form/NewsForm.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/adapter/form/NewsForm.java
@@ -1,37 +1,37 @@
 package jp.redmine.redmineclient.adapter.form;
 
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.TextView;
 
 import jp.redmine.redmineclient.R;
 import jp.redmine.redmineclient.activity.handler.WebviewActionInterface;
 import jp.redmine.redmineclient.entity.RedmineNews;
 import jp.redmine.redmineclient.form.helper.FormHelper;
-import jp.redmine.redmineclient.form.helper.WebViewHelper;
+import jp.redmine.redmineclient.form.helper.TextViewHelper;
 
 public class NewsForm extends FormHelper {
-	public WebView webView;
+	public TextView textView;
 	public TextView textSubject;
-	public WebViewHelper webViewHelper;
+	public TextViewHelper textViewHelper;
 	public NewsForm(View activity){
 		this.setup(activity);
 	}
 
 
 	public void setup(View view){
-		webView = (WebView)view.findViewById(R.id.webView);
+		textView = (TextView)view.findViewById(R.id.textView);
 		textSubject = (TextView)view.findViewById(R.id.textSubject);
 	}
 
 	public void setupWebView(WebviewActionInterface act){
-		webViewHelper = new WebViewHelper();
-		webViewHelper.setup(webView);
-		webViewHelper.setAction(act);
+		textViewHelper = new TextViewHelper();
+		textViewHelper.setup(textView);
+		textViewHelper.setAction(act);
 	}
 
 	public void setValue(RedmineNews jr){
-		webViewHelper.setContent(webView, jr.getConnectionId(), jr.getProject().getId(), jr.getDescription());
+		if (textViewHelper != null)
+			textViewHelper.setContent(textView, jr.getConnectionId(), jr.getProject().getId(), jr.getDescription());
 		textSubject.setText(jr.getTitle());
 	}
 

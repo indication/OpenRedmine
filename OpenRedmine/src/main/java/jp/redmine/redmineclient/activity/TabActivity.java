2c7c1e274090228f164073f2c7ced1e457cc326f
in_dow <in_dow@hotmail.com>
2015-02-14 03:58:34 +0900

    Implement NavigationDrawer

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/activity/TabActivity.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/activity/TabActivity.java
@@ -25,71 +25,215 @@ import jp.redmine.redmineclient.activity.handler.TimeentryActionInterface;
 import jp.redmine.redmineclient.activity.handler.WebviewActionInterface;
 import jp.redmine.redmineclient.activity.helper.ActivityHelper;
 import jp.redmine.redmineclient.activity.pager.CorePage;
-import jp.redmine.redmineclient.activity.pager.CorePager;
 import jp.redmine.redmineclient.fragment.ActivityInterface;
 
 abstract class TabActivity<T extends OrmLiteSqliteOpenHelper> extends OrmLiteFragmentActivity<T>
 	implements ActivityInterface {
+
+	/**
+	 * Remember the position of the selected item.
+	 */
+	private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+
+	/**
+	 * Per the design guidelines, you should show the drawer on launch until the user manually
+	 * expands it. This shared preference tracks this.
+	 */
+	private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
+
+	/**
+	 * Helper component that ties the action bar to the navigation drawer.
+	 */
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerListView;
+	private View mFragmentContainerView;
+	private SharedPreferences mPreferences;
+	private List<CorePage> mListTabs;
+
+	private int mCurrentSelectedPosition = 0;
+	private boolean mFromSavedInstanceState;
+	private boolean mUserLearnedDrawer;
+
 	abstract protected List<CorePage> getTabs();
 	public TabActivity(){
 		super();
 	}
+	private Toolbar mActionBarToolbar;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		ActivityHelper.setupTheme(this);
+		setContentView(R.layout.navigation_list);
+		setUp();
 		super.onCreate(savedInstanceState);
+
+
 		final ActionBar actionBar = getSupportActionBar();
-		setContentView(R.layout.page_fragment_pager);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
 
-		List<CorePage> list = getTabs();
+		mListTabs = getTabs();
+		mDrawerListView = (ListView)findViewById(R.id.listMenu);
 
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-		actionBar.setDisplayShowTitleEnabled(true);
-
-		final ViewPager mPager = (ViewPager) findViewById(R.id.pager);
-
-		/** Defining a listener for pageChange */
-		ViewPager.SimpleOnPageChangeListener pageChangeListener = new ViewPager.SimpleOnPageChangeListener(){
+		mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
-			public void onPageSelected(int position) {
-				super.onPageSelected(position);
-				actionBar.setSelectedNavigationItem(position);
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				selectItem(position);
+			}
+		});
+
+		BaseAdapter adapter = new BaseAdapter() {
+			@Override
+			public int getCount() {
+				return mListTabs.size();
+			}
+
+			@Override
+			public Object getItem(int position) {
+				return mListTabs.get(position);
+			}
+
+			@Override
+			public long getItemId(int position) {
+				return position;
+			}
+
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				if(convertView == null) {
+					convertView = getLayoutInflater().inflate(R.layout.listitem_navigation, null);
+				}
+				CorePage page = mListTabs.get(position);
+				ImageView image = (ImageView)convertView.findViewById(R.id.imageView);
+				TextView text= (TextView)convertView.findViewById(R.id.textSubject);
+				if(page != null){
+					Drawable icon = getResources().getDrawable(
+							page.getIcon() == null ? android.R.drawable.ic_menu_gallery : page.getIcon()
+					);
+					image.setImageDrawable(icon);
+					text.setText(page.getName());
+				}
+				return convertView;
 			}
 		};
-
-		/** Setting the pageChange listner to the viewPager */
-		mPager.setOnPageChangeListener(pageChangeListener);
-		mPager.setAdapter(new CorePager(getSupportFragmentManager(), list));
-
-		ActionBar.TabListener listener = new ActionBar.TabListener() {
+		mDrawerListView.setAdapter(adapter);
+		adapter.notifyDataSetChanged();
+		mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
-			public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-				mPager.setCurrentItem(tab.getPosition());
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				CorePage page = (CorePage) parent.getItemAtPosition(position);
+
+				mActionBarToolbar.setTitle(page.getName());
+				mActionBarToolbar.setNavigationIcon(page.getIcon() == null ? R.drawable.ic_launcher : page.getIcon());
+				getSupportFragmentManager().beginTransaction()
+						.replace(R.id.container, page.getFragment())
+						.commit();
+
+				mDrawerLayout.closeDrawer(mFragmentContainerView);
 			}
+		});
+		mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
 
-			@Override
-			public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+		// Read in the flag indicating whether or not the user has demonstrated awareness of the
+		// drawer. See PREF_USER_LEARNED_DRAWER for details.
+		mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		mUserLearnedDrawer = mPreferences.getBoolean(PREF_USER_LEARNED_DRAWER, false);
 
-			}
-
-			@Override
-			public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
-			}
-		};
-
-		for(CorePage item : list){
-			ActionBar.Tab tab = actionBar.newTab();
-			tab.setText(item.getName());
-			tab.setTabListener(listener);
-			if (item.getIcon() != null)
-				tab.setIcon(item.getIcon());
-			actionBar.addTab(tab, item.isDefault());
+		if (savedInstanceState != null) {
+			mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+			mFromSavedInstanceState = true;
 		}
 
+		// Select either the default item (0) or the last selected item.
+		selectItem(mCurrentSelectedPosition);
+		mDrawerListView.performItemClick(mDrawerListView,mCurrentSelectedPosition,0);
+
+	}
+
+	public boolean isDrawerOpen() {
+		return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(mFragmentContainerView);
+	}
+
+	protected Toolbar getActionBarToolbar() {
+		if (mActionBarToolbar == null) {
+			mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+			if (mActionBarToolbar != null) {
+				setSupportActionBar(mActionBarToolbar);
+			}
+		}
+		return mActionBarToolbar;
+	}
+	/**
+	 * Users of this fragment must call this method to set up the navigation drawer interactions.
+	 *
+	 */
+	public void setUp() {
+		mFragmentContainerView = findViewById(R.id.listMenu);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+		// set a custom shadow that overlays the main content when the drawer opens
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+		// set up the drawer's list view with items and click listener
+
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the navigation drawer and the action bar app icon.
+		mDrawerToggle = new ActionBarDrawerToggle(
+				this,                    /* host Activity */
+				mDrawerLayout,                    /* DrawerLayout object */
+				getActionBarToolbar(),
+				R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
+				R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
+		) {
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+
+				supportInvalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+			}
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+
+				if (!mUserLearnedDrawer) {
+					// The user manually opened the drawer; store this flag to prevent auto-showing
+					// the navigation drawer automatically in the future.
+					mUserLearnedDrawer = true;
+					mPreferences.edit().putBoolean(PREF_USER_LEARNED_DRAWER, true).commit();
+				}
+
+				supportInvalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+			}
+		};
+
+		// If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
+		// per the navigation drawer design guidelines.
+		if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
+			mDrawerLayout.openDrawer(mFragmentContainerView);
+		}
+
+		// Defer code dependent on restoration of previous instance state.
+		mDrawerLayout.post(new Runnable() {
+			@Override
+			public void run() {
+				mDrawerToggle.syncState();
+			}
+		});
+
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+	}
+
+	private void selectItem(int position) {
+		mCurrentSelectedPosition = position;
+		if (mDrawerListView != null) {
+			mDrawerListView.setItemChecked(position, true);
+		}
+		if (mDrawerLayout != null) {
+			mDrawerLayout.closeDrawer(mFragmentContainerView);
+		}
 	}
 
 	@SuppressWarnings("unchecked")

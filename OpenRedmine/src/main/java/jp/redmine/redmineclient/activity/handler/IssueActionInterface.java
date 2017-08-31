d7f2e2176596da2536fae6c6d4c0ee21df785dc7
in_dow <in_dow@hotmail.com>
2017-08-31 23:37:36 +0900

    Fix lint warnings

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/activity/handler/IssueActionInterface.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/activity/handler/IssueActionInterface.java
@@ -1,11 +1,11 @@
 package jp.redmine.redmineclient.activity.handler;
 
 public interface IssueActionInterface {
-	public void onIssueFilterList(int connectionid, int filterid);
-	public void onIssueList(int connectionid, long projectid);
-	public void onKanbanList(int connectionid, long projectid);
-	public void onIssueSelected(int connectionid, int issueid);
-	public void onIssueEdit(int connectionid, int issueid);
-	public void onIssueRefreshed(int connectionid, int issueid);
-	public void onIssueAdd(int connectionId, long projectId);
+	void onIssueFilterList(int connectionid, int filterid);
+	void onIssueList(int connectionid, long projectid);
+	void onKanbanList(int connectionid, long projectid);
+	void onIssueSelected(int connectionid, int issueid);
+	void onIssueEdit(int connectionid, int issueid);
+	void onIssueRefreshed(int connectionid, int issueid);
+	void onIssueAdd(int connectionId, long projectId);
 }

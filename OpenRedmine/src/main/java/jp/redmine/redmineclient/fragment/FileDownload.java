33caaa01d5cc066d23204a3ab32521ad4c8924b8
in dow <in_dow@hotmail.com>
2019-10-27 17:42:11 +0900

    Support for showing images
    - Refactor attachment
    - Show image on download form and move it
    
    Thank you for the solution:
     https://stackoverflow.com/questions/14814542/moving-imageview-with-touch-event

--- OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/FileDownload.java
+++ OpenRedmine/src/main/java/jp/redmine/redmineclient/fragment/FileDownload.java
@@ -14,13 +14,11 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
@@ -33,6 +31,7 @@ import jp.redmine.redmineclient.db.cache.DatabaseCacheHelper;
 import jp.redmine.redmineclient.db.cache.RedmineAttachmentModel;
 import jp.redmine.redmineclient.entity.RedmineAttachment;
 import jp.redmine.redmineclient.entity.RedmineConnection;
+import jp.redmine.redmineclient.entity.TypeConverter;
 import jp.redmine.redmineclient.fragment.form.DownloadForm;
 import jp.redmine.redmineclient.model.ConnectionModel;
 import jp.redmine.redmineclient.param.AttachmentArgument;
@@ -52,16 +51,17 @@ public class FileDownload extends OrmLiteFragment<DatabaseCacheHelper> {
 		return instance;
 	}
 
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.page_download, container, false);
 	}
-
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		form = new DownloadForm(getView());
+		form = new DownloadForm(getActivity(), getView());
+
 		form.buttonDownload.setOnClickListener(v -> {
 			AttachmentArgument instance = new AttachmentArgument();
 			instance.setArgument(getArguments());
@@ -71,10 +71,10 @@ public class FileDownload extends OrmLiteFragment<DatabaseCacheHelper> {
 
 				Intent intent = new Intent(Intent.ACTION_VIEW);
 				Uri uri = Attachment.getUrl(attachment.getId());
-				intent.setData(uri);
+				String mime = TypeConverter.getMimeType(attachment.getFilenameExt());
+				intent.setDataAndType(uri, mime);
 				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
 				getActivity().startActivity(intent);
-
 			} catch (ActivityNotFoundException e) {
 				Toast.makeText(getActivity(), R.string.activity_not_found, Toast.LENGTH_SHORT).show();
 			} catch (SQLException e) {
